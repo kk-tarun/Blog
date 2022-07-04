ebd04bba6dddec91d82bc826a8e5277836dbe3e0
Tarun Kumar Kushwaha <74929799+kk-tarun@users.noreply.github.com>
2022-07-04 19:43:04 +0530

    added template

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Form from "./components/Form";
+
+const App = ()=>{
+  return (
+    <div className="ParentContainer">
+      <NavBar />
+      <Form />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
